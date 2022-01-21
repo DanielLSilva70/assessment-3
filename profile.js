10ccbe2306b0cb2ef134abf25483f416a09228cf
Daniel Silva <silvaldaniel70@gmail.com>
2022-01-21 12:05:28 -0600

    styles

--- profile.js
+++ profile.js
@@ -1,1 +1,16 @@
-document.getElementById('color').addEventListener('click', 'color')
+
+let favColor = document.querySelector('#color');
+let favPlace = document.querySelector('#place');
+let favRitual = document.querySelector('#ritual');
+
+favColor.addEventListener('click', () => {
+    alert('My favorite color is blue');
+})
+
+favPlace.addEventListener('click', () => {
+    alert('My favortie place is Brazil');
+})
+
+favRitual.addEventListener('click', () => {
+    alert('My favorite ritual is eating cinnamon toast crunch in the morning');
+})
